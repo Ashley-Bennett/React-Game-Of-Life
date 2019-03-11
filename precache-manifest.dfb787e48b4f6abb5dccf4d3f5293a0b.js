049ecb5ba063d34652396b05fbd3813acab0df44
Ashley Bennett <codenation@CN-20.local>
2019-03-11 15:48:10 +0000

    Updates

---
+++ precache-manifest.dfb787e48b4f6abb5dccf4d3f5293a0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "293188c5aa931dc2b92e",
+    "url": "/React-Game-Of-Life/static/js/runtime~main.293188c5.js"
+  },
+  {
+    "revision": "8ebcab50b391572cc3ba",
+    "url": "/React-Game-Of-Life/static/js/main.8ebcab50.chunk.js"
+  },
+  {
+    "revision": "13721b33fdf601528450",
+    "url": "/React-Game-Of-Life/static/js/2.13721b33.chunk.js"
+  },
+  {
+    "revision": "8ebcab50b391572cc3ba",
+    "url": "/React-Game-Of-Life/static/css/main.44d76563.chunk.css"
+  },
+  {
+    "revision": "849a9e14003b95cc5a087260439efc7e",
+    "url": "/React-Game-Of-Life/index.html"
+  }
+];
